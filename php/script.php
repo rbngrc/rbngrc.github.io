5080d506e9d09bd4fbad288f4580b565e8b034dc
Ruben Garcia <rubengarciarbngrc@gmail.com>
2020-08-24 22:17:55 +0200

    Minor Change

--- php/script.php
+++ php/script.php
@@ -11,7 +11,7 @@
 
     $to = "rubengarciarbngrc@gmail.com";
     $headers = "From: $email_from \r\n";
-    $headers = "Reply-To: $email \r\n";
+    // $headers = "Reply-To: $email \r\n";
 
     mail($to, $email_subject, $email_body, $headers);
     header("Location: form.html");
