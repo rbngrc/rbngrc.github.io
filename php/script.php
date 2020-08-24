fe31b4e4d98b6bb0db8967a4f0fcd9e0d59a440a
Ruben Garcia <rubengarciarbngrc@gmail.com>
2020-08-24 22:31:32 +0200

    Update script.php

--- php/script.php
+++ php/script.php
@@ -2,8 +2,8 @@
 if(isset($_POST['btn-yellow-text'])){
     $to = "rubengarciarbngrc@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
     $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['subject'];
