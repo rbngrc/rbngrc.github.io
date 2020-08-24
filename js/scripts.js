73c691e789eee7dc60c657ec0e77f69c924d48c6
Ruben Garcia <rubengarciarbngrc@gmail.com>
2020-08-24 22:42:49 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 function sendEmail() {
 	Email.send({
 	Host: "smtp.gmail.com",
-	Username : "<sender’s email address>",
-	Password : "<email password>",
-	To : '<recipient’s email address>',
-	From : "<sender’s email address>",
-	Subject : "<email subject>",
+	Username : "rbngarcia60@gmail.com",
+	Password : "NN5gZbfLB4jsFxBKGE3E",
+	To : 'rubengarciarbngrc@gmail.com',
+	From : "rbngarcia60@gmail.com",
+	Subject : "WEB FORM",
 	Body : "<email body>",
 	}).then(
 		message => alert("mail sent successfully")
