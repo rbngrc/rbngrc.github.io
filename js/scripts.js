850a891a244a9451f210cff3c0faf005866ee94f
Ruben Garcia <rubengarciarbngrc@gmail.com>
2020-08-25 13:59:19 +0200

    Funcional form!
    Finally the form is up and running, it has one little bug, but it's working!

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
-//index.js  
-function sendEmail() {
-	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "rbngarcia60@gmail.com",
-	Password : "NN5gZbfLB4jsFxBKGE3E",
-	To : 'rubengarciarbngrc@gmail.com',
-	From : "rbngarcia60@gmail.com",
-	Subject : "WEB FORM",
-	Body : "<email body>",
-	}).then(
-		message => alert("mail sent successfully")
-	);
+const init = function() {
+	document.getElementById('btn-form-text').addEventListener('click', send)
 }
+
+const send = function(ev){
+
+	ev.preventDefault();
+
+	var fname = document.getElementById("fname").value
+	var lname = document.getElementById("lname").value
+	var email = document.getElementById("email").value
+	var subject = document.getElementById("subject").value
+
+	Email.send({
+		Host: "smtp.elasticemail.com",
+		Username: "rbngrcdzwebpage@gmail.com",
+		Password: "66A6D510C31C0CAC7C08DF5F6B6CC27898C4",
+		From: "rbngrcdzwebpage@gmail.com",
+		To: 'rubengarciarbngrc@gmail.com',
+		Subject: "EMAIL DE LA WEB",
+		Body: `${fname} ${lname} con correo ${email} te ha escrito a la web para decirte: ${subject}`
+	})
+	.then(function (message) {
+		alert("Sent successfully. I will get in touch as soon as possible :)")
+	});
+
+	document.getElementById('contact-form').reset();
+}
+
+document.addEventListener('DOMContentLoaded', init);
