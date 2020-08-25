9796a03683a55161bf34f8743b7415d8a6ac1a23
Ruben Garcia <rubengarciarbngrc@gmail.com>
2020-08-25 14:31:42 +0200

    solucion de bug empty form

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,27 @@ const send = function(ev){
 
 	ev.preventDefault();
 
-	var fname = document.getElementById("fname").value
-	var lname = document.getElementById("lname").value
-	var email = document.getElementById("email").value
-	var subject = document.getElementById("subject").value
+	const fname = document.getElementById("fname").value
+	const lname = document.getElementById("lname").value
+	const email = document.getElementById("email").value
+	const subject = document.getElementById("subject").value
 
-	Email.send({
-		Host: "smtp.elasticemail.com",
-		Username: "rbngrcdzwebpage@gmail.com",
-		Password: "66A6D510C31C0CAC7C08DF5F6B6CC27898C4",
-		From: "rbngrcdzwebpage@gmail.com",
-		To: 'rubengarciarbngrc@gmail.com',
-		Subject: "EMAIL DE LA WEB",
-		Body: `${fname} ${lname} con correo ${email} te ha escrito a la web para decirte: ${subject}`
-	})
-	.then(function (message) {
-		alert("Successfully sent. I will get in touch as soon as possible :)")
-	});
+	if (fname === '' || lname === '' || email === '' || subject === '') {
+		alert("Please fill the form")
+	} else {
+		Email.send({
+			Host: "smtp.elasticemail.com",
+			Username: "rbngrcdzwebpage@gmail.com",
+			Password: "66A6D510C31C0CAC7C08DF5F6B6CC27898C4",
+			From: "rbngrcdzwebpage@gmail.com",
+			To: 'rubengarciarbngrc@gmail.com',
+			Subject: "EMAIL DE LA WEB",
+			Body: `${fname} ${lname} con correo ${email} te ha escrito a la web para decirte: ${subject}`
+		})
+		.then(function (message) {
+			alert("Successfully sent. I will get in touch as soon as possible :)")
+		});
+	}
 
 	document.getElementById('contact-form').reset();
 }
